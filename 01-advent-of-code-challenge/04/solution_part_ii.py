9045691419d306f353b0724783f44cbaa3f5732e
WesleyKasteller <wesleykcarlesso@edu.unifil.br>
2023-02-26 20:34:07 -0300

    04 part 02

--- 01-advent-of-code-challenge/04/solution_part_ii.py
+++ 01-advent-of-code-challenge/04/solution_part_ii.py
@@ -1,6 +1,23 @@
 import sys
 
 list = []
+sum = 0
 
 for line in sys.stdin:
     list.append(line.rstrip())
+
+for idx, i in enumerate(list): 
+    arr = i.split(',')
+    arr2 = [int(n) for n in arr[0].split('-')]
+    arr3 = [int(n) for n in arr[1].split('-')]
+
+    if (arr3[0] >= arr2[0] and arr3[0] <= arr2[1]):
+        sum += 1
+    elif (arr3[1] >= arr2[0] and arr3[1] <= arr2[1]):
+        sum += 1
+    elif (arr2[0] >= arr3[0] and arr2[0] <= arr3[1]):
+        sum += 1
+    elif (arr2[1] >= arr3[0] and arr2[1] <= arr3[1]):
+        sum += 1
+
+print(sum)
